34d21cee14f7874be3bbba97212d37c244633315
eyal7775 <eyal.naftalovich@mdclone.com>
2022-11-03 14:37:48 +0200

    final stage

--- json_format.py
+++ json_format.py
@@ -3,7 +3,10 @@ import requests # pip install requests
 import os
 import json
 import datetime
+import urllib3
 
+start = datetime.datetime.now()
+urllib3.disable_warnings()
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"
 }
@@ -17,8 +20,10 @@ session.headers.update(headers)
 response = session.get(url, verify=False)
 html_page = response.content.decode('latin1')
 json_data = json.loads(html_page)
-with open(os.getcwd() + '\\' + file_name + "_" + timestamp + ".txt", "w+") as file:
+with open('/usr/app/src/' + file_name + "_" + timestamp + ".txt", "w") as file:
     file.write(json.dumps(json_data, indent=4))
+end = datetime.datetime.now()
+print("total runtime: " + str(end - start))
 
 # docker build . -t test -f Dockerfile.dockerfile
-# docker run -e FILE_NAME=logfilename -e MY_IP_SERVER=10.0.2.30 test
+# docker run -v $PWD/:/usr/app/src/ -e FILE_NAME=logfilename -e MY_IP_SERVER=10.0.2.30 test
